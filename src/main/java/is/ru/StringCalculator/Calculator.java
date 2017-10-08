34cfbd74a2d96c1be52658aef23037834f7ed39f
smaring7 <sng16@hotmail.com>
2017-10-08 17:31:00 +0000

    Refactoring

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -7,8 +7,12 @@ public class Calculator {
 			return 0;
 		else if(text.contains(",")) {
 			String numbers[] = text.split(",");
-			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+			return toInt(numbers[0]) + toInt(numbers[1]);
 		}
 		return 1;
+	}
+	
+	private static int toInt(String number) {
+		return Integer.parseInt(number);
 	}
 }
